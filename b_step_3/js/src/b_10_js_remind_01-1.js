50cee2c8ad63ff64dd0b997c316e86773cf984f4
mittwoch01 <wenigwaves@gmail.com>
2021-11-16 19:35:14 +0900

    복습 추가

--- b_step_3/js/src/b_10_js_remind_01-1.js
+++ b_step_3/js/src/b_10_js_remind_01-1.js
@@ -49,8 +49,81 @@ console.log(b+1,c+1); // 문자, 문자
 a = parseInt(a %= 3); // 10/3일 경우 몫은 3 나머지는 1
 console.log(a);
 
-(a > 2)?(a = 10):(a = 20);
-console.log(a);
+(a > 2)?(a = 10):(a = 20); // a의 값이 1이니까 거짓! 그래서 거짓일때 a = 20이 도출
+console.log(a === 20);
+
+//---------------------------------------
+var arr = [1,2,3,4,5,6,7,8,9,0]; // 순서가 있는 값
+var obj = {'a':1, 'b':2, 'c':3}; // 순서가 없다. 정의를 내릴 때 쓴다. {객체객체객체}
+console.log(arr[4], obj.b, obj['b']);
+
+// --------------------------------------
+var fn = function(e){
+  return e + b;
+};
+var fnCon = fn(arr[3]);
+console.log(fnCon);
+
+//---------------------------------------
+// if(){}else{}
+// switch(){}
+// for(){} , for-in , .forEach
+// function(){}
+
+// switch(){}
+var switchN = 6;
+var data;
+switch(switchN){
+  case 1:
+    data = '마우스 왼쪽버튼을 클릭하였습니다.'
+    break;
+  case 2:
+    data = '마우스 오른쪽버튼을 클릭하였습니다.'
+    break;
+  case 3:
+    data = '마우스 휠을 클릭하였습니다.'
+    break;
+  case 4:
+    data = '마우스 측면버튼을 클릭하였습니다.'
+    break;
+  default:
+    data = '마우스 기타 다른버튼을 클릭하였습니다.'
+    break;
+}
+
+console.log(data);
+
+var arr2 = [1,2,3,4,5,6,7,8,43,2,4,6,66];
+// for(최초; 비교; 증감){}
+for(var j=0; j<10; j++){
+  console.log('j:', j, '-', arr2[j]);
+}
+
+// [].forEach(function(각 요소 하나씩, 순서){})
+arr2.forEach(function(arg, idx){
+  console.log(idx,':', arg, arr2[idx]);
+});
+
+// for(임의변수k in 객체){}
+console.log(obj);
+for(var k in obj){
+  console.log(obj[k]);
+}
+// ---------------------------
+
+var an = [1,3,4];
+var na = [1,3,4];
+console.log(an === na); // false
+var n = 5;
+var u = 5;
+console.log( n === u); // true
+
+for(var l = 0; l<3; l+=1){
+  console.log( an[l] === na[l] );
+}
+
+var nna = na;
+console.log(nna === na);
 
 
 
--- b_step_3/js/src/b_10_js_remind_01-2.js
+++ b_step_3/js/src/b_10_js_remind_01-2.js
@@ -1,3 +1,94 @@
 // b_10_js_remind_01-2.js
+// console.log(i);
 
-console.log(i);
+
+var ar1 = ['딸기', '포도', '바나나','오렌지'];
+var cAr = [];
+
+var arFn = function(arr){
+  var arEx = [];
+  arr.forEach(function(data,index){
+    arEx[index] = data;  
+  });
+  return arEx;
+}
+
+cAr = arFn(ar1);
+
+ar1.push('melon');
+console.log(ar1);
+console.log(cAr);
+
+console.clear();
+
+/*
+var obt = {'a':1, 'b':2};
+var cObt = {};
+for( var prop in obt ){
+  // console.log(prop);
+  cObt[prop] = obt[prop];
+}
+obt.c = 3;
+console.log(obt, cObt);
+*/
+
+var ob1 = {
+  'fruits': ['딸기', '포도', '바나나','오렌지'],
+  'drink' : 'coffee',
+  'ade' : {'ice':'lemonade'}
+};
+var cOb1 = {};
+
+/*
+for(var prop in ob1){
+
+  // Array.isArray(ob1[prop]) -> 배열의 내용이 맞는지 파악
+  // ob1[prop].constructor === Array -> 배열의 내용이 맞는지 파악
+
+  if(ob1[prop].constructor === Array){
+  // ob1의 프로퍼티에 들어있는 배열을 확인하여 복사
+    cOb1[prop].forEach(function(data,i){
+      cOb1[prop] = [];
+      cOb1[prop][i] = data
+    });
+  }else{
+    // cOb1에 대한 객체를 체크하여 복사
+    cOb1[prop] = ob1[prop];
+  }
+}
+*/
+
+for(var prop in ob1){
+  if(ob1[prop].constructor === Array){
+    cOb1[prop] = arFn(ob1[prop]);
+  }else{
+    cOb1[prop] = ob1[prop];
+  }
+}
+
+
+ob1.car = 'niro';
+ob1.fruits.push('melon');
+console.log(cOb1);
+
+//------------------------------------------------
+console.clear();
+
+var originAr = { // 문자가 아니다 {}<< 가 있으니까 .. 안에 있는것들만이 문자 // 얘는JSON형식의 배열이 아니다?
+  'fruits': ['딸기', '포도', '바나나','오렌지'],
+  'drink' : 'coffee',
+  'ade' : {'ice':'lemonade'}
+};
+var textAr = JSON.stringify(originAr); // JSON형식을 보이는 그대로 전체를 문자화 처리
+var copyAr = JSON.parse(textAr); // JSON형식(객체/배열)으로 변환
+
+originAr.more = '추가객체 삽입'
+originAr.fruits.push('감');
+
+console.log( copyAr );
+console.log( originAr );
+
+// JSON(JavaScript Object Notation) 형식의 기초
+// 1. property, value 모두 쌍따옴표로 처리 (별도저장시)
+// 2. 주석노노
+// 3. 객체와 배열들의 집합
