bef89620d04bae538cb605979814097d89df9d65
mittwoch01 <wenigwaves@gmail.com>
2021-11-22 11:16:23 +0900

    prototype

--- b_step_3/js/src/b_13_js_constructor.js
+++ b_step_3/js/src/b_13_js_constructor.js
@@ -50,3 +50,51 @@ console.log( alpha.alpha );
 
 // this
 
+console.clear();
+// --------------------------------
+
+var ul = document.querySelector('ul');
+var list = ul.children;
+console.log( list );
+
+// prototype은 재설정, 추가 옵션을 주는 개념
+// 배열이 가지고 있는 본연의 기능 중 값을 배열형식으로 처리하는 것
+var listArr = Array.prototype.slice.apply( list );
+console.log( listArr );
+
+// list.forEach(function(data, index){
+//   data.style.border = '1px solid #3af'
+// });
+// ----------------------------------------------
+
+// prototype는 하나의 기능을 사용하 수 있는 환경을 구축하는 처리형태
+// 사용하는 변수 값 자체에 있는 기능은 아니고, 본연의 타입형태에 담겨 처리할 수 있도록 만드는 것.
+
+// --------------------------------------------------------------------------
+// 배열 형식을 가진 기능이 실제 배열이 아닌 형태는 유사배열로 불리고,
+// 이는 배열의 고유 기능을 처리하지 못한다.(배열 메소드 기능)
+// 그렇기에 강제로 배열로 처리하기 위해서는 배열의 고유기능을 처리해주어야 하는데
+// 이때 필요한 형식이 prototype이다. (고유기능을 이요하여 강제 수행)
+
+// 이처럼, 본래 가지고 있지 않는 형식을 강제로 수행하기 위해서는 어떠한 설정을 주어야 하는데
+// 이를 prototype에 내장시켜 처리 하도록 만든다.
+
+// 생성자 함수,
+// this,
+// 배열/유사배열
+// prototype
+
+
+// 배열 메소드
+var arr = [];
+// console.log( arr.push(1) );
+// console.log( arr.unshift('data') );
+
+var ListFn = function(type, color){
+  this.type = type;
+  this.color = color;
+};
+ListFn.prototype.store = 'anyang'; // 강제로 방출  = anyang밖에 안나옴
+
+var coffee = new ListFn('americano','brown');
+console.log( coffee.store );
