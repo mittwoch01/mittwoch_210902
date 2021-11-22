35e869a389153ecd08c6d458a968675d531e5e16
mittwoch01 <wenigwaves@gmail.com>
2021-11-22 17:38:45 +0900

    constructor

--- b_step_3/js/src/b_13_js_constructor.js
+++ b_step_3/js/src/b_13_js_constructor.js
@@ -1,4 +1,7 @@
 // b_13_js_constructor
+'use strict'; // 엄격하게 기능을 제한하여 처리하는 모드
+
+// 생성자 함수를 제작하려면, 함수의 이름을 PascalCase기법으로 처리
 
 // var user = {};
 // user.name = 'op';
@@ -30,10 +33,10 @@ console.log( user2.email );
 UserSetting.prototype.group = '가족 명단 체크';
 
 
-console.log( user1.group2 );
+console.log( user1.group2 );  // group2를 찾을수 없음
 
-UserSetting.prototype.trueCheck = function(){ // prototype은 기능을 추가하는 것?
-  return this.age / 2; 
+UserSetting.prototype.trueCheck = function(){ // prototype은 기능을 추가하는 것?  
+  return this.age / 2;  // 
 };
 
 console.log(user1.trueCheck());
@@ -98,3 +101,113 @@ ListFn.prototype.store = 'anyang'; // 강제로 방출  = anyang밖에 안나옴
 
 var coffee = new ListFn('americano','brown');
 console.log( coffee.store );
+
+// ---------------------------------------------
+// console.clear();
+
+
+// 1. this : window
+// 2. this : 생성자 함수로 만들어진 객체
+// 3. this : function(){} - window이지만, 엄격한 모드('use strict')로 전환시 undefined
+// 4. this : method 처리시 객체로 처리되어있는 변수명
+// 5. this : 이벤트 핸들러 - 이벤트의 주체가 되는 선택자
+
+var n = 0;
+var addFn = function(){
+  // 'use strict' : 모드를 사용하면, 일반함수내의 this는 undefined를 가리킨다.
+  // this.n = 50;
+  n = 50;
+  n++;
+};
+addFn();
+
+console.log(n);
+console.log( window.n );
+
+// -----------------------
+var lastName = 'cho';
+var firstName = 'wave';
+
+var useFile = {
+  firstName : 'wave',
+  lastName : 'jo',
+  job : 'studient',
+  subJob : 'artist',
+  fullName : function(){
+    return this.lastName + ' ' + this.firstName;
+  }
+};
+console.log( useFile.fullName() );
+// -------------------------------------
+// method : call, apply, bind
+
+// new Array() === [];
+var ar1 = [];
+ar1.push('banana', 'kiwi');
+
+// Array, Object, Function
+Array.prototype.unshift.call(ar1, 'orange', 'watermelon');
+
+console.log( ar1 );
+
+var obj = {
+  string : 'wave',
+  reName : function(){
+    console.log( 'name : ', this.string);
+  }
+};
+
+obj.reName();
+
+var obj2 = {
+  string : 'sub name'
+};
+
+console.log( obj2.string );
+obj.reName.call(obj2);
+
+// ----------------------------------
+var listFn = function(){
+  // return arguments;
+  // return Array.prototype.join.call(arguments).split(',');
+  return Array.prototype.join.call(arguments);
+};
+
+var makeList = listFn('test', 'file', 1,2,5,7,10);
+console.log( makeList );
+
+
+
+// -------------------------------------
+var btn = document.querySelector('#btn'); // #btn = this
+btn.addEventListener('click', function(e){
+  console.log( this );
+});
+
+// --------------------------------------------------------
+
+var Phone = function(brand, product){
+  this.brand = brand;
+  this.product = product;
+};
+// var setFn = function(brand, product){
+//   return this.brand + this.product;
+// }
+
+
+Phone.prototype.set = function(version, year){
+  console.log( this.brand, this.product, version, year);
+}
+
+var brand = 'apple';
+var product = 'iphone';
+
+var br1 = new Phone('samsung', 'gallaxy');
+// console.log( br1 );
+br1.set('s21', 2021); // samsung gallaxy s21 2021 ?????
+br1.set.call(this, 's21', 2021); // samsung gallaxy s21 2021
+br1.set.apply(this, ['s21', 2021]); // samsung gallaxy s21 2021
+var reset = br1.set.bind(this, 's21' ,2021);
+reset(); // samsung gallaxy s21 2021
+
+// call 은 value 담기 , apply는 array 담기
