201114ff9d44df0d64386e8ab3d040e8e1bc5765
mittwoch01 <wenigwaves@gmail.com>
2021-12-02 10:37:07 +0900

    tabmenu2

---
+++ b_step_3/js/src/c_10_dom_tab_menu2-1.js
@@ -0,0 +1,8 @@
+// 
+// c_10_dom_tab_menj_2-1.js
+
+var innerCode = '<a href="#" data-id><h4 class="event_title"></h4><p class="event_narration"></p><dl class="date"><dt class="blind">기간</dt><dd></dd></dl><dl class="event_check success"><dt></dt><dd></dd></dl></a>';
+
+
+var li1 = document.querySelector('.event_particle li');
+li1.innerHTML = innerCode;
