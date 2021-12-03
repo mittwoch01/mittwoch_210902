10571e25f9b247f856fbce9778b5cff7c8c53ebc
mittwoch01 <wenigwaves@gmail.com>
2021-12-03 10:25:50 +0900

    객체 data dom처리 card내용

--- b_step_3/js/src/c_11_dom_insert_data.js
+++ b_step_3/js/src/c_11_dom_insert_data.js
@@ -69,10 +69,72 @@ var makeLi;
 // data에도 var i = 0; 을 사하고 (i + 1)에도 사용한다.
 // innerText는 중복을 허용하지 않고 내부자료를 삭제한 후 덮어씌운다. 그래서 append를 사용한다.
 // append는 기존 자료에 새 자료를 삽입하는 메소드이다.
-
+/*
 for(; i < 2; i+=1){
   makeLi = document.createElement('li');
   // makeLi.innerHTML = (i + 1) + data[i].title + ' ' + '<br />' + data[i].contents + ' ';
   makeLi.innerHTML = '<div class="bg_img" style="background-image:url(' + data[i].bgImg + ')"><span class="blind">' + data[i].bgNarr + '</span></div><dl><dt>' + data[i].title + '</dt><dd><span>' + data[i].contents + '</span><br /><a href=\"' + data[i].link + '\" target=\"_blank\">자세히 보기</a></dd></dl>';
   cardUl.appendChild(makeLi);
 }
+*/
+
+// 우선 기본 코드를 모두 담은 후에 필요한 부분만 색출해서 수정/첨부/삭제
+var baseCode = '<div class="bg_img"><span class="blind"></span></div>\
+                <dl>\
+                  <dt></dt>\
+                  <dd>\
+                    <span></span>\
+                    <br />\
+                    <a target="_blank">자세히 보기</a>\
+                  </dd>\
+                </dl>';
+
+for(;i<len;i++){
+  makeLi = document.createElement('li');
+  makeLi.innerHTML = baseCode;
+  cardUl.appendChild(makeLi);
+}
+
+// 1번쨰 내용만 우선 정리해서 삽입
+// data[0].
+// bgImg: background-image, bgNarr: 설명 
+// title: 제목
+// content: 내용, link: 주소
+
+// 2번째 
+// 1. li요소 첫번쨰 선택
+// 2. 공통 n번째를 가리키는 변수를 생성
+var li = cardUl.querySelectorAll('li');
+
+var setCardfn = function(n){
+  var idx = n;
+  var dt = li[idx].querySelector('dt');
+  var narr = li[idx].querySelector('dd>span');
+  var link = li[idx].querySelector('dd>a');
+  var bgImg = li[idx].querySelector('.bg_img');
+  var bgNarr = bgImg.querySelector('span');
+
+  dt.innerText = data[idx].title;
+  narr.innerText = data[idx].contents;
+  // link.href = data[idx].link; // html 상에 존재하는 property에 값을 처리할때 사용할수 있다.
+  // link.setAttribute('href', data[idx].link); // 어떠한 속성도 처리 가능 data - 속성
+  ( !!data[idx].link ) ? link.href = data[idx].link : link.remove();
+
+  if(data[idx].bgImg !== undefined){
+    bgImg.style.backgroundImage = 'url('+ data[idx].bgImg + ')';
+    bgNarr.innerText = data[idx].bgNarr;
+  }else{
+    bgImg.remove();
+  }
+}; //setCardfn();
+
+setCardfn(0);
+setCardfn(1);
+setCardfn(2);
+setCardfn(3);
+setCardfn(4);
+setCardfn(5);
+setCardfn(6);
+
+  // <a data-href(사용자 임의 속성)="추후 무언가 어떤한 기능을 하기위해 임의로 담아놓은 값"></a>
+  // <a href="#" data-send="??">인증</a>
