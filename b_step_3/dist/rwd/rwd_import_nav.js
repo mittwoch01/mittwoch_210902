b2ecd4af67ba19f6b229e9f9d49c512b041f4159
mittwoch01 <wenigwaves@gmail.com>
2021-12-24 11:04:12 +0900

    [add] nav :ab: data 분리 및 각 내용에 맞춰서 내용 불러오기

---
+++ b_step_3/dist/rwd/rwd_import_nav.js
@@ -0,0 +1,22 @@
+// rwd_import_nav.js
+
+{
+  const path = '../temp/';
+  const data = { // 유사배열? 배열아님
+    mob : path + 'mobile_rwd_nav.html',
+    laptop : path + 'laptop_rwd_nav.html'
+  }
+  const elNav = document.querySelector('#navBox');
+
+  const fnDeviceNav = (type)=>{
+
+    fetch(type) 
+    .then(response => response.text())
+    .then(element=>{
+      elNav.innerHTML = element;
+    })
+  };
+
+  fnDeviceNav(data.laptop); // laptop을 썼기 떄문에 'laptop_rwd_nav.html만' 나온다.
+
+}
