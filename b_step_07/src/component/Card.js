0b5e3880aac2ce427a7f1dda0fe0128448dfdef8
mittwoch01 <wenigwaves@gmail.com>
2022-01-11 12:52:19 +0900

    [add]React:ab:파일나누기, 연결하기, scss설정하기

---
+++ b_step_07/src/component/Card.js
@@ -0,0 +1,17 @@
+
+import "../style/Card.scss";
+
+export default function (){
+  return(
+    <div className="card">
+      <div className="img">이미지 삽입</div>
+      <dl>
+        <dt>title</dt>
+        <dd>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.di
+        </dd>
+      </dl>
+      <div className="btn"></div>
+    </div>
+  )
+}
