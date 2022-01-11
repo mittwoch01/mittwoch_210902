72a56f6ad72911a32f5f94d3e701be4b30926a94
mittwoch01 <wenigwaves@gmail.com>
2022-01-11 12:53:51 +0900

    [add]newBox:ab:close btn 삭제, 삼항연산자 사용예정

--- e_stylenanda/dist/src/s_00_newBox_language_modal.js
+++ e_stylenanda/dist/src/s_00_newBox_language_modal.js
@@ -11,7 +11,7 @@ const langLi = langList.querySelector('.mob_button > .five');
 const langModal = langIndi.querySelector('.mob_language_inner');
 
 // close btn
-const langCloseBtn = langModal.querySelector('.langCloseBtn');
+// const langCloseBtn = langModal.querySelector('.langCloseBtn');
 
 // 이벤트 처리
 langLi.addEventListener('click', e => {
@@ -19,9 +19,8 @@ langLi.addEventListener('click', e => {
   langModalArea.classList.add('on');
 });
 
-langCloseBtn.addEventListener('click', e =>  {
-  e.preventDefault();
-  langModalArea.classList.remove('on');
-});
+// 삼항연산자(true, false)를 이용해서
+// 변수설정 contains 해놓은 다음 
+// classList contains(string) 
 
 // 이벤트 처리 다른 방법
